dc769aa72cea78de41f9ffd1c7f2e2e0c633589e
miarworker <miarworker@gmail.com>
2022-05-10 23:30:28 +0400

    add db.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
-import {Module} from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import {Module} from "@nestjs/common";
+import {UserModule} from './user/user.module';
+import {ServiceModule} from './service/service.module';
+import {SubscriptionModule} from './subscription/subscription.module';
+import {DbModule} from "./db/db.module";
 
 @Module({
-    imports: [TypeOrmModule.forRoot()],
+    imports: [UserModule, ServiceModule, SubscriptionModule, DbModule],
 })
 export class AppModule {
 }
